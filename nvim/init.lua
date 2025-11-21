3fb9bbddb9d4f9d3522a1fec108125bf3f5da32f
Tristan Carter <91004717+tristan-carter@users.noreply.github.com>
2025-11-21 18:12:17 +0000

    Enhance comments for clarity in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,19 +15,19 @@ require("lazy").setup({
   { "morhetz/gruvbox" },
   { "catppuccin/nvim", name = "catppuccin" },
 
-  -- Treesitter
+  -- Treesitter (Syntax Highlighting)
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
   -- Status line
   { "nvim-lualine/lualine.nvim" },
 
-  -- Autopairs
+  -- Autopairs (Automatic closing brackets)
   { "windwp/nvim-autopairs" },
 
   -- File tree
   { "kyazdani42/nvim-tree.lua" },
 
-  -- Git
+  -- Git Integration
   { "tpope/vim-fugitive" },
   { "lewis6991/gitsigns.nvim" },
 
@@ -38,20 +38,22 @@ require("lazy").setup({
   { "nvim-lua/plenary.nvim" },
   { "nvim-telescope/telescope.nvim", tag = "0.1.5" },
 
-  -- LSP + Completion
+  -- LSP (Language Server Protocol)
   { "neovim/nvim-lspconfig" },
+  
+  -- Autocompletion Engine
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-nvim-lsp", -- Connects LSP to CMP
+      "hrsh7th/cmp-buffer",   -- Suggest words from current buffer
+      "hrsh7th/cmp-path",     -- Suggest file paths
+      "L3MON4D3/LuaSnip",     -- Snippets engine
       "saadparwaiz1/cmp_luasnip",
     },
   },
 
-  -- Rust
+  -- Rust Specialized Plugin
   {
     "mrcjkb/rustaceanvim",
     version = "^4",
@@ -62,7 +64,7 @@ require("lazy").setup({
 -- ── General settings ────────────────────────────────
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
 vim.cmd[[colorscheme gruvbox]]
 
 -- ── Treesitter ──────────────────────────────────────
@@ -85,21 +87,8 @@ require("lualine").setup {
 -- ── Autopairs ───────────────────────────────────────
 require("nvim-autopairs").setup {}
 
--- ── LSP (Rust + General) ────────────────────────────
-local lspconfig = require("lspconfig")
-
--- For non-Rust languages
-lspconfig.pyright.setup({})
-lspconfig.clangd.setup({})
-lspconfig.lua_ls.setup({
-  settings = {
-    Lua = { diagnostics = { globals = { "vim" } } }
-  }
-})
-
--- rust-analyzer is auto-managed by rustaceanvim
-
 -- ── nvim-cmp setup (Autocompletion) ─────────────────
+-- We setup CMP *before* LSP to ensure capabilities are ready
 local cmp = require("cmp")
 cmp.setup({
   snippet = {
@@ -111,12 +100,43 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping.select_prev_item(),
   }),
   sources = {
-    { name = "nvim_lsp" },
+    { name = "nvim_lsp" }, -- Must be first to prioritize code intelligence
     { name = "buffer" },
     { name = "path" },
     { name = "luasnip" },
   },
 })
+
+-- ── LSP (Language Server Protocol) ──────────────────
+local lspconfig = require("lspconfig")
+
+-- Get the capabilities from cmp-nvim-lsp
+-- This tells the servers "Hey, I can support autocompletion!"
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- List of servers to setup (excluding Rust, which is handled by rustaceanvim)
+local servers = { 'pyright', 'clangd', 'lua_ls' }
+
+for _, lsp in ipairs(servers) do
+  local opts = {
+    capabilities = capabilities, -- Pass completion capabilities
+  }
+
+  -- Special config for Lua to recognize the 'vim' global variable
+  if lsp == "lua_ls" then
+    opts.settings = {
+      Lua = {
+        diagnostics = { globals = { "vim" } },
+        workspace = {
+             library = vim.api.nvim_get_runtime_file("", true),
+        },
+      }
+    }
+  end
+
+  -- Initialize the server
+  lspconfig[lsp].setup(opts)
+end
 
 -- ── Telescope ───────────────────────────────────────
 require("telescope").setup {}
@@ -129,4 +149,3 @@ vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]])
 vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-w>j]])
 vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]])
 vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-w>l]])
-
